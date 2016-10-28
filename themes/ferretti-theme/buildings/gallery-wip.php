18e93a4118ef0c158e3cf14d1eaaf4f3b1e9ff33
tedc <egrandinetti@gmail.com>
2016-10-28 16:06:25 +0200

    gallery wip

--- themes/ferretti-theme/buildings/gallery-wip.php
+++ themes/ferretti-theme/buildings/gallery-wip.php
@@ -1,16 +1,21 @@
 <?php if(have_rows('gallery')) : ?>
     <?php while(have_rows('gallery')) : the_row(); $images = get_sub_field('fotografie'); ?>
-        <h2><?php echo __('Lavori', 'frt'); ?></h2>
+    <div class="frt_wip">
+        <div class="corner frt_corner_content"></div>
+        <h2 class="frt_title_wip"><?php echo __('Lavori', 'frt'); ?></h2>
         <?php if($images) : ?>
-        <ul>
+        <ul class="frt_wip_list">
             <?php foreach($images as $img) : ?>
-                <li style="background-image: url(<?php echo $img['url']; ?>)">
-                    <img class="img-hidden" alt="<?php echo $img['alt']; ?>" src="<?php echo $img['url']; ?>" />
-                    <?php echo $img['caption']; ?>
-                    <?php echo $img['description']; ?>
+                <li class="frt_wip_list_item" style="background-image: url(<?php echo $img['url']; ?>)">
+<!--                    <img class="img-hidden" alt="<?php echo $img['alt']; ?>" src="<?php echo $img['url']; ?>" />-->
+                    <div class="container_quote">
+                        <?php echo $img['caption']; ?>
+                        <?php echo $img['description']; ?>
+                    </div>
                 </li>
             <?php endforeach; ?>
         </ul>
         <?php endif; ?>
+    </div>
     <?php endwhile; ?>
 <?php endif; ?>
